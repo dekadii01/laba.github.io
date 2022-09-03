b328cc11dc4927c16011b508927fc3aebd8a6466
dekadii01 <109329655+dekadii01@users.noreply.github.com>
2022-09-03 18:18:26 +0700

    Delete source directory

--- source/js/app.js
+++
@@ -1,9 +0,0 @@
-const toggle = document.getElementById('toggle-nav');
-const navItems = document.getElementById('nav-content');
-const body = document.getElementById('bekgron');
-
-toggle.addEventListener('click', function() {
-    navItems.classList.toggle('toggle-on');
-    navItems.classList.toggle('fade-in');
-    body.classList.toggle('background-image');
-})
